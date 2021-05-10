c33012c3dfaed781b809d54d9834d0593a9d66c4
fandong <admin@fandong.me>
2021-05-10 17:22:48 +0800

    带壳截屏

--- Tools/Controller/Tools/ScreenShotViewController.swift
+++ Tools/Controller/Tools/ScreenShotViewController.swift
@@ -72,23 +72,16 @@ class ScreenShotViewController: BaseViewController {
         return scrollView
     }()
     
-    func generateShot(imageName: String, originImage: UIImage, radius: CGFloat){
-        
-        let radiusImage = originImage.roundImage(cornerRadi: radius)
-        
+    func generateShot(imageName: String, originImage: UIImage){
         let iphoneImg = UIImage(named: imageName)
         let iphoneImgW = iphoneImg?.size.width ?? 0
         let iphoneImgH = iphoneImg?.size.height ?? 0
         
-        let maskImg = UIImage(named: "iPhone 11 Mask.jpeg")
-        let maskImgW = maskImg?.size.width ?? 0
-        let maskImgH = maskImg?.size.height ?? 0
-        
-        let realRect = CGRect(x: (iphoneImgW - maskImgW ) / 2, y: (iphoneImgH - maskImgH ) / 2, width: maskImgW, height: maskImgH)
+        let realRect = CGRect(x: 142, y: 180, width: originImage.size.width, height: originImage.size.height)
         
         UIGraphicsBeginImageContext(CGSize(width: iphoneImgW, height: iphoneImgH))
+        originImage.draw(in: realRect)
         iphoneImg?.draw(in: CGRect(x: 0, y: 0, width: iphoneImgW, height: iphoneImgH))
-        radiusImage?.draw(in: realRect)
         let resultImg = UIGraphicsGetImageFromCurrentImageContext() ?? UIImage()
         UIGraphicsEndImageContext()
         
@@ -102,57 +95,8 @@ extension ScreenShotViewController : UIImagePickerControllerDelegate,UINavigatio
         picker.dismiss(animated: true, completion: nil)
         let image = info[.originalImage] as! UIImage
         
-        var iphoneImgName = ""
+        let iphoneImgName = Device.current.safeDescription + " Silver"
         
-        let device = Device.current
-        if device.isFaceIDCapable {
-            if device == .iPhone12 || device == .iPhone12Pro || device == .iPhone12ProMax {
-                let alert = UIAlertController(title: "选择不同款式", message: nil, preferredStyle: .actionSheet)
-                alert.addAction(UIAlertAction(title: "iPhone 12 黑", style: .default, handler: { (action) in
-                    iphoneImgName = "12-Black.png"
-                    self.generateShot(imageName: iphoneImgName, originImage: image, radius: 90)
-                }))
-                alert.addAction(UIAlertAction(title: "iPhone 12 蓝", style: .default, handler: { (action) in
-                    iphoneImgName = "12-Blue.png"
-                    self.generateShot(imageName: iphoneImgName, originImage: image, radius: 90)
-                }))
-                alert.addAction(UIAlertAction(title: "iPhone 12 绿", style: .default, handler: { (action) in
-                    iphoneImgName = "12-Green.png"
-                    self.generateShot(imageName: iphoneImgName, originImage: image, radius: 90)
-                }))
-                alert.addAction(UIAlertAction(title: "iPhone 12 红", style: .default, handler: { (action) in
-                    iphoneImgName = "12-Red.png"
-                    self.generateShot(imageName: iphoneImgName, originImage: image, radius: 90)
-                }))
-                alert.addAction(UIAlertAction(title: "iPhone 12 白", style: .default, handler: { (action) in
-                    iphoneImgName = "12-White.png"
-                    self.generateShot(imageName: iphoneImgName, originImage: image, radius: 90)
-                }))
-                alert.addAction(UIAlertAction(title: "iPhone 12 Pro 黑", style: .default, handler: { (action) in
-                    iphoneImgName = "12P-Black.png"
-                    self.generateShot(imageName: iphoneImgName, originImage: image, radius: 90)
-                }))
-                alert.addAction(UIAlertAction(title: "iPhone 12 Pro 蓝", style: .default, handler: { (action) in
-                    iphoneImgName = "12P-Blue.png"
-                    self.generateShot(imageName: iphoneImgName, originImage: image, radius: 90)
-                }))
-                alert.addAction(UIAlertAction(title: "iPhone 12 Pro 金", style: .default, handler: { (action) in
-                    iphoneImgName = "12P-Gold.png"
-                    self.generateShot(imageName: iphoneImgName, originImage: image, radius: 90)
-                }))
-                alert.addAction(UIAlertAction(title: "iPhone 12 Pro 白", style: .default, handler: { (action) in
-                    iphoneImgName = "12P-White.png"
-                    self.generateShot(imageName: iphoneImgName, originImage: image, radius: 90)
-                }))
-                alert.addAction(UIAlertAction(title: "取消", style: .cancel, handler: nil))
-                present(alert, animated: true, completion: nil)
-            }else{
-                iphoneImgName = "iPhone 11.jpeg"
-                generateShot(imageName: iphoneImgName, originImage: image, radius: 70)
-            }
-        }else{
-            self.view.makeToast("由于无相关机型素材，目前仅支持全面屏设备")
-            return
-        }
+        generateShot(imageName: iphoneImgName, originImage: image)
     }
 }

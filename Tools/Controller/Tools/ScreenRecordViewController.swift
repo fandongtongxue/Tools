358e50fd9ff4b2e003b70ef5256d07b15b398175
fandong <admin@fandong.me>
2021-05-11 18:12:08 +0800

    fix

--- Tools/Controller/Tools/ScreenRecordViewController.swift
+++ Tools/Controller/Tools/ScreenRecordViewController.swift
@@ -64,84 +64,58 @@ class ScreenRecordViewController: BaseViewController {
     }()
     
     func getImagesFromVideo(mediaURL: URL){
-        let asset = AVAsset(url: mediaURL)
-        let imageGenerator = AVAssetImageGenerator(asset: asset)
-        imageGenerator.appliesPreferredTrackTransform = true
-        let duration = CMTimeGetSeconds(asset.duration)
-        var valueArray = [NSValue]()
-        for i in 0...Int64(duration*25) {
-            let point = CMTimeMake(value: i, timescale: 25) as! NSValue
-            valueArray.append(point)
-        }
-        var imageArray = [UIImage]()
-        var index = 0
-        view.makeToastActivity(.center)
-        imageGenerator.generateCGImagesAsynchronously(forTimes: valueArray) { (requestedTime, image, actualTime, result, error) in
-            if result == .succeeded && image != nil {
-                let finalImage = UIImage(cgImage: image!)
-                
-                let iphoneImgName = Device.current.safeDescription + " Silver"
-                let iphoneImg = UIImage(named: iphoneImgName)
-                let iphoneImgW = iphoneImg?.size.width ?? 0
-                let iphoneImgH = iphoneImg?.size.height ?? 0
-                
-                let realRect = CGRect(x: (iphoneImgW - finalImage.size.width) / 2, y: (iphoneImgH - finalImage.size.height) / 2, width: finalImage.size.width, height: finalImage.size.height)
-                
-                UIGraphicsBeginImageContext(CGSize(width: iphoneImgW, height: iphoneImgH))
-                finalImage.draw(in: realRect)
-                iphoneImg?.draw(in: CGRect(x: 0, y: 0, width: iphoneImgW, height: iphoneImgH))
-                let resultImg = UIGraphicsGetImageFromCurrentImageContext() ?? UIImage()
-                UIGraphicsEndImageContext()
-                
-                let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first! as NSString
-                
-                let date = Date()
-                let dateFormatter = DateFormatter()
-                dateFormatter.dateFormat = "yyyy_MM_dd_HH_mm_ss"
-                let dateStr = dateFormatter.string(from: date)
-                
-                let documentFilePath = documentDirectory.appendingPathComponent(dateStr+".mp4")
-                
-                let cachesDirectory = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first! as NSString
-                
-                var cachesFilePath = cachesDirectory.appendingPathComponent("image0.jpg")
-                let cachesFileDPath = cachesDirectory.appendingPathComponent("image%d.jpg")
-                do {
-                    cachesFilePath = cachesDirectory.appendingPathComponent("image"+"\(index)"+".jpg")
-                    try resultImg.jpegData(compressionQuality: 1)?.write(to: URL(fileURLWithPath: cachesFilePath))
-                    index = index + 1
-                    debugPrint(cachesFilePath)
-                } catch _ {
-                    
-                }
-                                
-                imageArray.append(resultImg)
-                
-                if imageArray.count == valueArray.count {
-                    let command = "-f image2 -i "+cachesFileDPath+" "+"-vcodec h264 -r 25"+" "+documentFilePath
-                    let session = FFmpegKit.executeAsync(command) { (session) in
-                        debugPrint("FFMpeg结束")
-                        DispatchQueue.main.async {
-                            self.view.hideToastActivity()
-                            debugPrint(documentFilePath)
-                            if UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(documentFilePath) {
-                                UISaveVideoAtPathToSavedPhotosAlbum(documentFilePath, self, #selector(self.video(videoPath:didFinishSavingWithError:contextInfo:)), nil)
-                            }
-                        }
-                    }
-                    let code = session?.getReturnCode()
-                    if ReturnCode.isSuccess(code) {
-                        debugPrint("ReturnCode.isSuccess(code)")
-                    }else if ReturnCode.isCancel(code){
-                        debugPrint("ReturnCode.isCancel(code)")
-                    }else{
-                        debugPrint("ReturnCode.isFailure(code)")
+        
+        let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first! as NSString
+        
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy_MM_dd_HH_mm_ss"
+        let dateStr = dateFormatter.string(from: date)
+        
+        let documentFilePath = documentDirectory.appendingPathComponent(dateStr+".mp4")
+        
+        let cachesDirectory = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first! as NSString
+        
+        var cachesFilePath = cachesDirectory.appendingPathComponent("image0.jpg")
+        let cachesFileDPath = cachesDirectory.appendingPathComponent("image%d.jpg")
+        
+        //先切割图片
+        let command = "-i"+""+" -r 1 -q:v 2 -f image2 "+cachesFileDPath
+        let session = FFmpegKit.executeAsync(command) { (session) in
+            debugPrint("FFMpeg结束")
+            //图片合成视频
+            let command2 = "-f image2 -i "+cachesFileDPath+" "+"-vcodec h264 -r 25"+" "+documentFilePath
+            let session2 = FFmpegKit.executeAsync(command) { (session) in
+                debugPrint("FFMpeg结束")
+                DispatchQueue.main.async {
+                    self.view.hideToastActivity()
+                    debugPrint(documentFilePath)
+                    if UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(documentFilePath) {
+                        UISaveVideoAtPathToSavedPhotosAlbum(documentFilePath, self, #selector(self.video(videoPath:didFinishSavingWithError:contextInfo:)), nil)
                     }
                 }
+            }
+            let code2 = session?.getReturnCode()
+            if ReturnCode.isSuccess(code2) {
+                debugPrint("ReturnCode.isSuccess(code)")
+            }else if ReturnCode.isCancel(code2){
+                debugPrint("ReturnCode.isCancel(code)")
             }else{
-                self.view.makeToast(error?.localizedDescription)
+                debugPrint("ReturnCode.isFailure(code)")
             }
         }
+        let code = session?.getReturnCode()
+        if ReturnCode.isSuccess(code) {
+            debugPrint("ReturnCode.isSuccess(code)")
+        }else if ReturnCode.isCancel(code){
+            debugPrint("ReturnCode.isCancel(code)")
+        }else{
+            debugPrint("ReturnCode.isFailure(code)")
+        }
+        
+        
+        
+        
     }
     
     @objc func video(videoPath: String, didFinishSavingWithError error: NSError, contextInfo info: AnyObject){

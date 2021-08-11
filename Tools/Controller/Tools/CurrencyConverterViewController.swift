53a0a04d53cf92561d38294d726171e8ca1a1b2f
fandongtongxue <admin@fandong.me>
2021-08-11 20:47:29 +0800

    汇率计算部分

---
+++ Tools/Controller/Tools/CurrencyConverterViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CurrencyConverterViewController.swift
+//  Tools
+//
+//  Created by 范东 on 2021/8/11.
+//
+
+import UIKit
+
+class CurrencyConverterViewController: BaseViewController {
+
+    var model = CurrencyConverterModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+
+    func requestData(){
+//        https://v6.exchangerate-api.com/v6/cf8304250ab3543002479bfd/latest/CNY
+        FDNetwork.GET(url: "https://v6.exchangerate-api.com/v6/cf8304250ab3543002479bfd/latest/CNY", param: [:]) { result in
+            self.model = CurrencyConverterModel.deserialize(from: result) ?? CurrencyConverterModel()
+        } failure: { error in
+            
+        }
+
+    }
+
+}
